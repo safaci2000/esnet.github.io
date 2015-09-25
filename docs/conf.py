d49f5105f03fd15a650fb426e35f6ff58f634b37
Jon Dugan <jdugan@x1024.net>
2015-09-25 12:51:22 -0500

    add new projects and bump copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ESnet Software'
-copyright = u'2014, ESnet'
+copyright = u'2014-2015, ESnet'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
